1b23e1e34b21950774614f85a10ffcc2f65eb134
KomaliReddyVavilala <161595708+KomaliReddyVavilala@users.noreply.github.com>
2025-09-02 12:31:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const level = document.getElementById("level");
+function findLevel() {
+	let counter = 0;
+	let currentElement = level;
+	while (currentElement!==null) {
+		currentElement = currentElement.parentNode;
+		counter++;
+	}
+	console.log("The level of the element is:",counter);
+}
